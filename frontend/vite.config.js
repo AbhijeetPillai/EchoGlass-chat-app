8dde3828aca3e82190eec13befa51d1a777c7b60
Abhijeet Pillai <pillaiabhijeetpillai@gmail.com>
2025-06-22 19:15:56 +0530

    project completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss(),],
+  plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, '../backend/public'), // move frontend build to backend/public
+  },
 })
-
